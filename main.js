8c808883bd4307b0329da1133aeeef64a4bb22da
Jean Luc <jlah@oxo.mu>
2023-04-08 16:11:18 +0400

    test

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ document.querySelector('#clear').addEventListener('click', ()=> {
 })
 
 const seven = () => {
-    console.log(this)
     if (document.querySelector('#display').value == '0') {
         document.querySelector('#display').value = '7'
     }
@@ -21,24 +20,103 @@ const seven = () => {
     }
 }
 
-const seven = () => {
-    console.log(this)
+const four = () => {
     if (document.querySelector('#display').value == '0') {
-        document.querySelector('#display').value = '7'
+        document.querySelector('#display').value = '4'
     }
 
     else{
-        document.querySelector('#display').value = document.querySelector('#display').value + '7'
+        document.querySelector('#display').value = document.querySelector('#display').value + '4'
     }
 }
 
-const seven = () => {
-    console.log(this)
+const one = () => {
     if (document.querySelector('#display').value == '0') {
-        document.querySelector('#display').value = '7'
+        document.querySelector('#display').value = '1'
     }
 
     else{
-        document.querySelector('#display').value = document.querySelector('#display').value + '7'
+        document.querySelector('#display').value = document.querySelector('#display').value + '1'
+    }
+}
+
+const zero = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '0'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '0'
+    }
+}
+
+const eight = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '8'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '8'
+    }
+}
+
+const five = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '5'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '5'
+    }
+}
+
+const two = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '2'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '2'
+    }
+}
+
+const nine = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '9'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '9'
+    }
+}
+
+const six = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '6'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '6'
+    }
+}
+
+const three = () => {
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = '3'
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '3'
+    }
+}
+
+const coma = () => {
+    console.log(this)
+    if (document.querySelector('#display').value == '0') {
+        document.querySelector('#display').value = ','
+    }
+
+    else{
+        document.querySelector('#display').value = document.querySelector('#display').value + '2'
     }
 }
